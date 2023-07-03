2742dcf78612e9599b28c1f17a78fc4190f56b8c
Masoud Sam <mas.sam1368@gmail.com>
2023-07-03 10:45:24 +0330

    working on router

--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { HashRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+console.log(import.meta.env.VITE_APP_BASE_URL);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <HashRouter>
+  <BrowserRouter basename={import.meta.env.VITE_APP_BASE_URL}>
     <React.StrictMode>
-
       <Routes>
        <Route path='/' element={<App />} />
-
       </Routes>
       <App />
     </React.StrictMode>
-  </HashRouter>
+  </BrowserRouter>
 );
