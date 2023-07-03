d7032b15b02e9b1b11d43ba241b8317fe92e210f
Masoud Sam <mas.sam1368@gmail.com>
2023-07-03 11:37:26 +0330

    ...

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/digikala',
+  base: '/digakala',
   build: {
     outDir: './build',
     rollupOptions: {
